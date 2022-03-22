deba787c06558ed9aa94ca47a859a463184c9324
Diego <diegojosecornago@gmail.com>
2022-03-21 22:46:37 -0300

    first commit

--- index.js
+++ index.js
@@ -8,18 +8,102 @@ const mysql = require('mysql2');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'))
+
+
+const conexion =  mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "GarabatO3331",
+    database: "metahapaxcontacto",
+});
+
+conexion.connect((error) =>{
+    if(error) throw error;
+    console.log('Conexión a la Data Base exitosa!!');
+});
+
 
 
 //Configuramos el Motor de Plantillas
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-hbs.registerPartials(path.join(__dirname, '../z-actividadfinal/views/partials'));
-app.use(express.static('./public'));
+hbs.registerPartials(path.join(__dirname, '/views/partials'));
 
 app.get('/', (req, res) =>{
     res.render('index')
 });
+
+
+
+
+
+app.get('/contactanos', (req, res) =>{
+    res.render('contactanos')
+});
+
+app.post('/formulario', (req, res) =>{
+
+    //Desestructuración de las variables
+    const { nombre, precio, descripcion } = req.body;
+        
+    if(nombre == "" || precio == ""){
+        
+        let validacion = 'Faltan datos para guardar el Producto';
+        
+        res.render('formulario', {
+            titulo: 'Formulario para Completar',
+            validacion
+        });
+
+    }else{
+
+        console.log(nombre);
+        console.log(precio);
+        console.log(descripcion);
+
+        //Insertar datos a la DB
+        let data = {
+            producto_nombre: nombre, 
+            producto_precio: precio,
+            producto_descripcion: descripcion
+        }
+
+        let sql = 'Insert into productos set ?';
+
+        conexion.query(sql, data, (error, results) =>{
+            if(error) throw error;
+            res.render('/', {
+            
+            }); 
+        })
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/merch', (req, res) =>{
+    res.render('merch')
+});
+
+
+//conexion.end();
+
 
 
 app.listen(Port, () => {
@@ -27,4 +111,4 @@ app.listen(Port, () => {
 });
 app.on('error', (err) =>{
     console.log(`Error en la ejecución del Servidor ${error}`);
-})
+});
