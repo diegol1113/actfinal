9a06959208c27f95b541782df9d5980e6d717f9b
Diego <diegojosecornago@gmail.com>
2022-03-28 10:07:45 -0300

    deplo3

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-//const Port = 8080;
 const path = require('path');
 const hbs = require('hbs');
 const mysql = require('mysql2');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
